1a4fd447af6297b40602aba38b3cf35c3a937756
Anson <ansonieung1086@gmail.com>
2018-02-21 22:56:40 +0800

    nothing

--- DBS/Featured Page/circularsWebViewController.swift
+++ DBS/Featured Page/circularsWebViewController.swift
@@ -101,9 +101,9 @@ class circularsWebViewController: UIViewController, TwicketSegmentedControlDeleg
         if arr.count != 0 {
             for i in 1...arr.count {
                 titles += ["#\(i)"]
+            }
         }
-        let frame = CGRect(x: self.view.frame.width / 2 - self.view.frame.width * 0.45 , y: self.view.frame.height - 55, width: self.view.frame.width * 0.9, height: 40)
-
+        let frame = CGRect(x: self.view.frame.width * 0.05 , y: self.view.frame.height - 55, width: self.view.frame.width * 0.9, height: 40)
         let segmentedControl = TwicketSegmentedControl(frame: frame)
         segmentedControl.setSegmentItems(titles)
         segmentedControl.delegate = self as? TwicketSegmentedControlDelegate
