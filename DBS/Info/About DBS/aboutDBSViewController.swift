905b2cb893b86ed9c8c3172f966f0ceec0e30176
Ben Lou <benelouie@gmail.com>
2018-03-04 22:31:21 +0800

    Bug Fixes

--- DBS/Info/About DBS/aboutDBSViewController.swift
+++ DBS/Info/About DBS/aboutDBSViewController.swift
@@ -174,16 +174,14 @@ class aboutDBSViewController: UIViewController {
         
         information.textAlignment = .justified
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+            self.vAndM(self)
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        vAndM(self)
     }
     
     func attributedText(_ str: String, _ titles: [String]) -> NSAttributedString {
