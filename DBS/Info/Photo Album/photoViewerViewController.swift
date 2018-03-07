d0ee2de0bbd5eb0faad53a77e29e82a8c627f4b5
Anson <ansonieung1086@gmail.com>
2018-03-07 16:52:36 +0800

    bug fixes

--- DBS/Info/Photo Album/photoViewerViewController.swift
+++ DBS/Info/Photo Album/photoViewerViewController.swift
@@ -210,12 +210,14 @@ class ImagePreviewFullViewCell: UICollectionViewCell, UIScrollViewDelegate {
             
             var scale = CGFloat(1)
             scale = (imgView.image?.size.width)! / frame.width
-            imgView.frame.size.height = (imgView.image?.size.height)! / scale
+            //imgView.frame.size.height = (imgView.image?.size.height)! / scale
+            print("double tap zoom in")
             
         } else {
-            scrollImg.setZoomScale(1.0, animated: true)
-            scrollImg.zoom(to: zoomRectForScale(scale: scrollImg.minimumZoomScale, center: recognizer.location(in: recognizer.view)), animated: false)
+            scrollImg.setZoomScale(1, animated: true)
+//            scrollImg.zoom(to: zoomRectForScale(scale: scrollImg.minimumZoomScale, center: recognizer.location(in: recognizer.view)), animated: false)
             imgView.frame = originalFrame
+            print("zoom out")
         }
     }
     
@@ -235,13 +237,16 @@ class ImagePreviewFullViewCell: UICollectionViewCell, UIScrollViewDelegate {
         return imgView
     }
     
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        if scrollView.zoomScale == 1{
+        var scale = CGFloat(1)
+        scale = (imgView.image?.size.width)! / frame.width
+        imgView.frame.size.height = (imgView.image?.size.height)! / scale
+        }
+    }
     
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        if scrollImg.zoomScale == 1{
-            var scale = CGFloat(1)
-            scale = (imgView.image?.size.width)! / frame.width
-            imgView.frame.size.height = (imgView.image?.size.height)! / scale
-        }
+        
         
         let offsetX = max((scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5, 0.0)
         let offsetY = max((scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5, 0.0)
