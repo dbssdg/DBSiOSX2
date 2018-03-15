f411f55f340bf1af77cd9bcfd2920cbca8d27da4
Kelvin Ng <kelvin01140@gmail.com>
2018-03-15 15:58:08 +0800

    4.0.2 Push

--- DBS/Info/ContributionsViewController.swift
+++ DBS/Info/ContributionsViewController.swift
@@ -25,7 +25,7 @@ class ContributionsViewController: UIViewController {
         
         let string = """
 Diocesan Boys' School Official School Application
-Version 4.0.1
+Version 4.0.2
 
 TEACHER-IN-CHARGE
 Mr. Chris Lee
