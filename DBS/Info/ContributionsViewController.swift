c0055a32c488ae23a2fd9dd4d8609fb78c2c31ce
Ben Lou <benelouie@gmail.com>
2018-03-21 07:59:18 +0800

    4.1

--- DBS/Info/ContributionsViewController.swift
+++ DBS/Info/ContributionsViewController.swift
@@ -25,7 +25,7 @@ class ContributionsViewController: UIViewController {
         
         let string = """
 Diocesan Boys' School Official School Application
-Version 4.0.3
+Version 4.1
 
 TEACHER-IN-CHARGE
 Mr. Chris Lee
